6e8f77cbb6b27e1191aabb7bb188556671807c99
evaitl <evaitl@gmail.com>
2020-11-19 00:16:44 -0500

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use std::io::{Read,Write};
-use std::fs::OpenOptions;
+use ed25519_dalek::Keypair;
+use std::fs::{File, OpenOptions};
+use std::io::{Read, Write};
 
 #[derive(Debug)]
 struct KSignerArgs {
     load: String, // fname to load a keypair from
     save: String, // fname to write keypair to
     public: bool, // Display public key
-    sign: bool, // Sign the files
+    sign: bool,   // Sign the files
     files: Vec<String>,
 }
 impl KSignerArgs {
@@ -49,80 +50,76 @@ or store a signature to a file for reuse later. ",
                  .multiple(true)
                  .help("Files to operate on"))
             .get_matches();
-        
+
         KSignerArgs {
             load: matches.value_of("load").unwrap_or("").to_string(),
             save: matches.value_of("save").unwrap_or("").to_string(),
             public: matches.is_present("public"),
             sign: matches.is_present("sign"),
-            files:
-            match matches.values_of("files") {
-                Some(iter) => iter.map(|s|s.to_string()).collect(),
+            files: match matches.values_of("files") {
+                Some(iter) => iter.map(|s| s.to_string()).collect(),
                 _ => vec![],
-            }
+            },
         }
     }
-    
 }
-use ed25519_dalek::Keypair;
-use std::fs::File;
 fn get_keypair(s: &str) -> Keypair {
-    if s=="" {
+    if s == "" {
         use rand::rngs::OsRng;
-        let mut csprng=OsRng{};
+        let mut csprng = OsRng {};
         Keypair::generate(&mut csprng)
     } else {
-        let r=File::open(s).expect("Couldn't open key file");
+        let r = File::open(s).expect("Couldn't open key file");
         serde_json::from_reader(r).expect("Read or parse error")
     }
 }
 
-
 fn process_files(sign: bool, kp: &Keypair, fns: &Vec<String>) {
-    use ed25519_dalek::{SIGNATURE_LENGTH, Signature, Signer};
+    use ed25519_dalek::{Signature, Signer, SIGNATURE_LENGTH};
     for fname in fns {
         if sign {
-            println!("Signing  {}",fname);
-            let mut r=File::open(fname).expect(&format!("Couldn't open file {}",fname));
-            let mut buf=Vec::new();
-            r.read_to_end(&mut buf).expect(&format!("Read error on {}",fname));
+            println!("Signing  {}", fname);
+            let mut r = File::open(fname).expect("Couldn't open file");
+            let mut buf = Vec::new();
+            r.read_to_end(&mut buf).expect("Read error on file");
             drop(r);
-            let sig=kp.sign(&buf);
-            let mut w=OpenOptions::new().append(true).open(fname)
-                .expect(&format!("Couln't append {}",fname));
+            let sig = kp.sign(&buf);
+            let mut w = OpenOptions::new()
+                .append(true)
+                .open(fname)
+                .expect("Couln't append to file");
             w.write_all(&sig.to_bytes()).expect("Write failure");
         } else {
-            println!("Checking: {}",fname);
-            let mut r=File::open(fname).expect("Couldn't open file");
-            let len=r.metadata().unwrap().len() as usize-SIGNATURE_LENGTH;
-            let mut buf=vec![0u8;len];
+            println!("Checking: {}", fname);
+            let mut r = File::open(fname).expect("Couldn't open file");
+            let len = r.metadata().unwrap().len() as usize - SIGNATURE_LENGTH;
+            let mut buf = vec![0u8; len];
             r.read_exact(&mut buf).expect("Read error");
-            let mut sigbuf= [0u8; SIGNATURE_LENGTH];
+            let mut sigbuf = [0u8; SIGNATURE_LENGTH];
             r.read_exact(&mut sigbuf).expect("Read sig error");
-            let sig=Signature::from(sigbuf);
-            let res=kp.verify(&buf,&sig);
+            let sig = Signature::from(sigbuf);
+            let res = kp.verify(&buf, &sig);
             if res.is_err() {
-                println!("Signature failed on {}: {:?}",fname,res);
-            } else{
-                println!("Signature valid for {}",fname);
+                println!("Signature failed on {}: {:?}", fname, res);
+            } else {
+                println!("Signature valid for {}", fname);
             }
         }
     }
 }
-fn save_keypair(sf: &str, kp: &Keypair){
-    if sf=="" {
+fn save_keypair(sf: &str, kp: &Keypair) {
+    if sf == "" {
         return;
     }
-    let w=File::create(sf).expect("Couldn't create key file");
-    serde_json::to_writer(w,kp).expect("Couldn't serialize keypair");
+    let w = File::create(sf).expect("Couldn't create key file");
+    serde_json::to_writer(w, kp).expect("Couldn't serialize keypair");
 }
 fn main() {
     let args = KSignerArgs::new();
-    println!("args: {:?}", args);
-    let kp= get_keypair(&args.load);
+    let kp = get_keypair(&args.load);
     if args.public {
-        println!("Public key: {}",serde_json::to_string(&kp.public).unwrap());
+        println!("Public key: {}", serde_json::to_string(&kp.public).unwrap());
     }
-    process_files(args.sign,&kp,&args.files);
+    process_files(args.sign, &kp, &args.files);
     save_keypair(&args.save, &kp);
 }
